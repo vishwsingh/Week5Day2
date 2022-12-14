3d9a51cc823fe9ed009e091dbe4801231d96eaa3
vishwsingh <khayekmusic@gmail.com>
2022-12-14 11:02:42 -0500

    Made updates

--- src/main/java/com/Week5Day2/department/Controller/DepartmentController.java
+++ src/main/java/com/Week5Day2/department/Controller/DepartmentController.java
@@ -1,14 +1,28 @@
 package com.Week5Day2.department.Controller;
 
+import com.Week5Day2.department.Entity.Department;
 import com.Week5Day2.department.Service.DepartmentService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/department")
+@RequestMapping("/departments")
+@Slf4j
 public class DepartmentController {
 
     @Autowired
     private DepartmentService departmentService;
+
+    @PostMapping("/")
+    public Department saveDepartment(@RequestBody Department department) {
+        log.info("Inside saveDepartment method of DepartmentController");
+        return departmentService.saveDepartment(department);
+    }
+
+    @GetMapping("/{id}")
+    public Department findByDepartmentId(@PathVariable("id") Long departmentId) {
+        log.info("Inside findByDepartmentId of DepartmentController");
+        return departmentService.findByDepartmentId(departmentId);
+    }
 }
--- src/main/java/com/Week5Day2/department/Service/DepartmentService.java
+++ src/main/java/com/Week5Day2/department/Service/DepartmentService.java
@@ -1,12 +1,25 @@
 package com.Week5Day2.department.Service;
 
+import com.Week5Day2.department.Entity.Department;
 import com.Week5Day2.department.Repository.DepartmentRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
+@Slf4j
 public class DepartmentService {
 
     @Autowired
     private DepartmentRepository departmentRepository;
+
+    public Department saveDepartment(Department department) {
+        log.info("Inside saveDepartment of DepartmentService");
+        return departmentRepository.save(department);
+    }
+
+    public Department findByDepartmentId(Long departmentId) {
+        log.info("Inside findByDepartmentId of DepartmentService");
+        return departmentRepository.findByDepartmentId(departmentId);
+    }
 }
